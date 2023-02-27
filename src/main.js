590ae29a28b1f0b73190189df578f715feb6fa72
noel <noeljose@gmail.com>
2023-02-28 00:22:36 +0530

    first commit

---
+++ src/main.js
@@ -0,0 +1,140 @@
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+const er = require("electron-reload")
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
+
+er(__dirname,{})
+
+const isDev = process.env.NODE_ENV !== 'production';
+const isMac = process.platform === 'darwin';
+
+let mainWindow;
+
+// Main Window
+function createMainWindow() {
+  mainWindow = new BrowserWindow({
+    width: 950,
+    height: 450,
+    // icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: false,
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+  }
+  });
+
+  // Show devtools automatically if in development
+  if (isDev) { mainWindow.webContents.openDevTools(); }
+
+   mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
+
+
+}
+
+
+
+// When the app is ready, create the window
+app.on('ready', () => {
+  createMainWindow();
+
+  // Remove variable from memory
+  mainWindow.on('closed', () => (mainWindow = null));
+});
+
+
+ipcMain.on('create-folder', async (event, folder) => {
+
+  let path = await dialog.showOpenDialog({
+    properties: ['openDirectory']
+  })
+
+  try {
+    if (path.canceled) return dialog.showErrorBox("Error", "Operation Canceled")
+
+    let masterFolderName = `${path.filePaths[0]}/${folder.name}`
+    fs.mkdirSync(masterFolderName)
+
+    switch (folder.type) {
+      case "VIDEO":
+        fs.mkdirSync(`${masterFolderName}/RENDERS`)
+        fs.mkdirSync(`${masterFolderName}/SOURCE`)
+        break;
+
+      case "VIDEO LOCALISATION":
+        fs.mkdirSync(`${masterFolderName}/RENDERS`)
+        folder.languages.forEach(language => {
+          fs.mkdirSync(`${masterFolderName}/RENDERS/${language}`)
+        });
+
+        fs.mkdirSync(`${masterFolderName}/SOURCE`)
+        folder.languages.forEach(language => {
+          fs.mkdirSync(`${masterFolderName}/SOURCE/${language}`)
+        });
+        break;
+
+        case "STATIC":
+          fs.mkdirSync(`${masterFolderName}/PSD`)
+          fs.mkdirSync(`${masterFolderName}/JPEG_PNG`)
+          fs.mkdirSync(`${masterFolderName}/RENDER_LOC`)
+          fs.mkdirSync(`${masterFolderName}/RENDER_LOC/DISPLAY ADS`)
+          fs.mkdirSync(`${masterFolderName}/RENDER_LOC/END CARDS`)
+          fs.mkdirSync(`${masterFolderName}/SOURCE_LOC`)
+          break;
+
+        case "STATIC LOCALISATION":
+          fs.mkdirSync(`${masterFolderName}/PSD`)
+          fs.mkdirSync(`${masterFolderName}/JPEG_PNG`)
+          fs.mkdirSync(`${masterFolderName}/RENDER_LOC`)
+          fs.mkdirSync(`${masterFolderName}/RENDER_LOC/DISPLAY ADS`)
+          fs.mkdirSync(`${masterFolderName}/RENDER_LOC/END CARDS`)
+          fs.mkdirSync(`${masterFolderName}/SOURCE_LOC`)
+
+          folder.languages.forEach(language => {
+            fs.mkdirSync(`${masterFolderName}/RENDER_LOC/END CARDS/${language}`)
+          });
+
+          folder.languages.forEach(language => {
+            fs.mkdirSync(`${masterFolderName}/RENDER_LOC/DISPLAY ADS/${language}`)
+          });
+          folder.languages.forEach(language => {
+            fs.mkdirSync(`${masterFolderName}/SOURCE_LOC/${language}`)
+          });
+          break;
+    
+      default:
+        break;
+    }
+    
+  } catch (error) {
+
+    return dialog.showErrorBox("Error", error.message)
+    
+  }
+
+  
+
+  
+
+
+    console.log({...folder, basePath: path.filePaths[0]});
+    
+  
+  
+ 
+  // Code to create folder here...
+});
+
+
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  if (!isMac) app.quit();
+});
+
+// Open a window if none are open (macOS)
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createMainWindow();
+});
+
---
+++ src/renderer/renderer.js
@@ -0,0 +1,76 @@
+const { ipcRenderer, dialog } = require('electron');
+
+let folder = {
+    name: "",
+    basePath: "",
+    languages: new Set([]),
+    type: "VIDEO",
+}
+const createFolderButton = document.getElementById('show-dialog-button');
+const createFolderButton2 = document.getElementById('show-dialog-button2');
+
+createFolderButton.addEventListener('click', () => {
+
+  ipcRenderer.send('create-folder', folder);
+});
+
+
+
+let languageContainer = document.getElementById("languages")
+let file = document.getElementById("file")
+let folderName = document.getElementById("folderName")
+let foldertype = document.getElementById("foldertype")
+
+let languages = ["ar_ZA", "fi_FI", "ko_KR", "es_MX", "ar_EG", "fr_FR", "ms_MY", "es_ES", "ca_ES", "fr_CA", "nb_NO", "es_419", "da_DK", "de_DE", "fa_IR", "sv_SE", "nl_NL", "el_GR", "pl_PL", "th_TH", "en_AU", "he_IL", "pt_BR", "zh_TW", "en_CA", "hi_IN", "pt_PT", "zh_HK", "en_IN", "hu_HU", "ro_RO", "tr_TR", "en_NZ", "id_ID", "ru_RU", "uk_UA", "en_GB", "it_IT", "sr_YU", "vi_VN", "en_US", "ja_JP", "zh_CN" ]
+
+
+
+function toggleLanguage(language) {
+
+    if (folder.languages.has(language)) 
+    {folder.languages.delete(language)}
+    
+    else 
+    {folder.languages.add(language)}
+
+    renderButtons()
+}
+
+function renderButtons(){
+
+    languageContainer.innerHTML = null
+    languages.forEach((language)=>{
+        let button = document.createElement("button")
+        button.innerText = language
+        button.style= `margin:2px; ${folder.languages.has(language)? " background-color:black;color:#fff;": ' background-color:unset'}`
+        if (folder.type == "VIDEO" || folder.type == "STATIC") {
+
+            button.disabled = true
+        }
+        button.addEventListener("click", ()=>toggleLanguage(language))
+        languageContainer.appendChild(button)
+    })
+
+    
+    
+}
+
+renderButtons()
+
+
+
+
+file.addEventListener("input", (e)=>{
+    console.log(e.target.files[0]);
+    folder.basePath = e.target.files[0].path
+})
+
+folderName.addEventListener("input", (e)=>{
+    folder.name = folderName.value
+})
+
+foldertype.addEventListener("input", (e)=>{
+    
+    folder.type = foldertype.value
+    renderButtons()
+})
